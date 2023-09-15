e1fcf80191db17a0dc2fdec0abb79b6778468a00
diegodevweb <diego.devwebb@gmail.com>
2023-09-15 16:27:54 -0300

    refactor: Ajuste na rota create

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::get('/', function () {
     return redirect('/supports');
 });
 
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+
 Route::get('/supports/{id}/', [SupportController::class, 'show'])->name('supports.show');
 
 Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
@@ -14,8 +16,6 @@ Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('su
 Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
 
 Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
