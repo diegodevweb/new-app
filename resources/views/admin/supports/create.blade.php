3a3fd1cd5b1ac10bf1fe6200f88c5e9064ccdfa8
diegodevweb <diego.devwebb@gmail.com>
2023-09-14 22:58:17 -0300

    refactor: Frontend da view create

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,8 +1,83 @@
-<h1>Nova duvida</h1>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<form action="{{ route('supports.store') }}" method="POST">
-    @csrf()
-    <input type="text" placeholder="Assunto" name="subject">
-    <textarea name="content" cols="30" rows="5" placeholder="Descricao"></textarea>
-    <button type="submit">Enviar</button>
-</form>
+    <title>Listagem de Suportes</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="flex justify-center items-start h-screen mt-10">
+
+
+<div class="flex flex-col justify-center items-center w-full max-w-5xl rounded-lg shadow-md p-6">
+    <h1 class="mb-6 text-center text-4xl">Nova dúvida</h1>
+    <form action="{{ route('supports.store') }}" method="POST">
+        @csrf
+        <div class="mb-4">
+            <input type="text" placeholder="Assunto" name="subject" class="border rounded px-2 py-1 w-full">
+        </div>
+        <div class="mb-4">
+            <textarea name="content" cols="30" rows="5" placeholder="Descrição" class="border rounded px-2 py-1 w-full"></textarea>
+        </div>
+        <div>
+            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Enviar</button>
+        </div>
+    </form>
+</div>
+
+</body>
+
+{{--<!DOCTYPE html>--}}
+{{--<html lang="pt-Br">--}}
+{{--<head>--}}
+{{--    <meta charset="utf-8">--}}
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1">--}}
+
+{{--    <title>Listagem de Suportes</title>--}}
+
+{{--    <!-- Fonts -->--}}
+{{--    <link rel="preconnect" href="https://fonts.bunny.net">--}}
+{{--    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>--}}
+{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
+{{--</head>--}}
+
+{{--<body class="flex justify-center items-center h-screen">--}}
+{{--<div class="w-full max-w-6xl rounded-lg shadow-md p-6 h-screen">--}}
+{{--    <h1 class="text-center text-4xl mb-4">Listagem de Suportes</h1>--}}
+{{--    <a href="{{ route('supports.create') }}" class="block text-center rounded-full p-2 my-2 bg-blue-500 text-white">Criar nova dúvida</a>--}}
+{{--    <table class="w-full border-collapse rounded-lg overflow-hidden table-auto max-10">--}}
+{{--        <thead>--}}
+{{--        <tr class="bg-gray-100">--}}
+{{--            <th class="p-4">Id</th>--}}
+{{--            <th class="p-4">Assunto</th>--}}
+{{--            <th class="p-4">Status</th>--}}
+{{--            <th class="p-4">Descrição</th>--}}
+{{--            <th class="p-4">Ações</th>--}}
+{{--        </tr>--}}
+{{--        </thead>--}}
+{{--        <tbody>--}}
+{{--        @foreach($supports as $support)--}}
+{{--            <tr class="bg-gray-50">--}}
+{{--                <td class="p-4">{{ $support->id }}</td>--}}
+{{--                <td class="p-4">{{ $support->subject }}</td>--}}
+{{--                <td class="p-4">{{ $support->status }}</td>--}}
+{{--                <td class="p-4">{{ $support->content }}</td>--}}
+{{--                <td class="p-4">--}}
+{{--                    <a href="{{ route('supports.show', $support->id) }}" class="ml-2 text-purple-500">Ver</a>--}}
+{{--                    <a href="{{ route('supports.edit', $support->id) }}" class="ml-2 text-blue-500">Editar</a>--}}
+{{--                    <a href="{{ route('supports.delete', $support->id) }}" class="ml-2 text-red-500">Deletar</a>--}}
+{{--                </td>--}}
+{{--            </tr>--}}
+{{--        @endforeach--}}
+{{--        </tbody>--}}
+{{--    </table>--}}
+{{--</div>--}}
+{{--</body>--}}
+{{--</html>--}}
+
