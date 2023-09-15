6f31eb8e6b1d832033190ee294c9660c6c5f9165
diegodevweb <diego.devwebb@gmail.com>
2023-09-14 21:54:38 -0300

    refactor: Paginação dos registros a serem exibidos na tela.

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -12,7 +12,7 @@ class SupportController extends Controller
 {
     public function index(Support $support)
     {
-        $supports = $support->all();
+        $supports = $support->paginate(12);
 
         return view('admin.supports.index', compact('supports'));
     }
